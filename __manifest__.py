5b32f9420857f0ae402afa738a548c366e8c5803
Samina Moin <saminamoinit@gmail.com>
2026-03-24 21:05:01 +0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+Python
+{
+    'name': 'Saudi ZATCA Phase 2 Compliance',
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'ZATCA Phase 2 Invoice Validation',
+    'author': 'Shameena Moin Qaisar',
+    'depends': ['account'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
