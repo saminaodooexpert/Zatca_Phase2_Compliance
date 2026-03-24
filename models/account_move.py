968203afca90242f0789982fedb38bb5b26b4e9a
Samina Moin <saminamoinit@gmail.com>
2026-03-24 21:13:49 +0500

    Create account_move.py

---
+++ models/account_move.py
@@ -0,0 +1,13 @@
+Python
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def action_post(self):
+        for move in self:
+            if move.move_type in ['out_invoice', 'out_refund']:
+                if not move.partner_id.vat:
+                    raise UserError(_("ZATCA Phase 2 Error: Customer VAT number is missing! Please add it before confirming the invoice."))
+        return super(AccountMove, self).action_post()
